fadda9b3b2cb45d24ebfb657e450db5e691540c2
hectorBWA <hectorzavala12345@gmail.com>
2018-07-17 13:42:24 -0700

    phase 3 assignment 1 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { FriendsComponent } from './friends/friends.component';
+
+// const appRoutes: Routes = [
+  
+//   {
+//     path: 'login', // localhost4200/recipe
+//     component: LoginComponent
+//   },
+//   {
+//     path: 'dashboard', // localhost4200/shopping
+//     component: DashboardComponent
+//   }
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    FriendsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: RegisterComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'friends', component: FriendsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
